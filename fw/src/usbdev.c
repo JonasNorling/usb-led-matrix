fbdbd513761aec1b71ef20f33a40c7564687f162
Jonas Norling <jonas@elemental.se>
2021-01-09 10:54:27 +0100

    HID descriptor

--- fw/src/usbdev.c
+++ fw/src/usbdev.c
@@ -5,7 +5,36 @@
 
 LOG_MODULE_REGISTER(usbdev);
 
-static const uint8_t hid_report_desc[] = HID_MOUSE_REPORT_DESC(2);
+#define LED_COUNT 64
+
+// Vendor defined HID report
+static const uint8_t hid_report_desc[] = {
+	/* Usage page: vendor defined */
+	HID_GLOBAL_ITEM(ITEM_TAG_USAGE_PAGE, 2), 0x00, 0xFF,
+
+	/* Usage: vendor specific */
+	HID_LI_USAGE, 0x01,
+	/* Collection: Application Collection */
+	HID_MI_COLLECTION, COLLECTION_APPLICATION,
+	/* Logical Minimum: 0 */
+	HID_GI_LOGICAL_MIN(1), 0x00,
+	/* Logical Maximum: 255 */
+	HID_GI_LOGICAL_MAX(1), 0xFF,
+	/* Report Size: 8 bits */
+	HID_GI_REPORT_SIZE, 0x08,
+	/* Report Count (in bytes) */
+	HID_GI_REPORT_COUNT, LED_COUNT,
+
+	/* Report ID: 1 */
+	HID_GI_REPORT_ID, 1,
+	/* Vendor Usage 2 */
+	HID_LI_USAGE, 0x02,
+	/* Output: Data, Variable, Absolute & Buffered bytes*/
+	HID_MI_OUTPUT, 0x86,
+
+	/* End collection */
+	HID_MI_COLLECTION_END,
+};
 
 static void status_cb(enum usb_dc_status_code status, const uint8_t *param)
 {
@@ -45,22 +74,5 @@ void usbdev_init(void)
     if (ret) {
         LOG_ERR("Failed to enable USB");
         return;
-    }
-
-    uint8_t report[4] = { 0x00 };
-    while (true) {
-        LOG_INF("Moving");
-        k_sleep(K_MSEC(10000));
-        report[1] = 1;
-        ret = hid_int_ep_write(hid_dev, report, sizeof(report), NULL);
-        if (ret) {
-            LOG_ERR("HID write error, %d", ret);
-        }
-        k_sleep(K_MSEC(100));
-        report[1] = 0;
-        ret = hid_int_ep_write(hid_dev, report, sizeof(report), NULL);
-        if (ret) {
-            LOG_ERR("HID write error, %d", ret);
-        }
     }
 }
