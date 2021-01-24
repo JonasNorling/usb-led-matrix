4622f5ae4e106866b75d286c31fbcbfe0af50eb5
Jonas Norling <jonas@elemental.se>
2021-01-24 13:47:10 +0100

    Use HSI

--- fw/src/main.c
+++ fw/src/main.c
@@ -10,12 +10,13 @@ static void enable_crs()
 {
 #if CONFIG_CLOCK_STM32_PLL_SRC_HSI
     __HAL_RCC_CRS_CLK_ENABLE();
+    LL_CRS_DeInit();
     LL_CRS_SetSyncDivider(LL_CRS_SYNC_DIV_1);
     LL_CRS_SetSyncPolarity(LL_CRS_SYNC_POLARITY_RISING);
     LL_CRS_SetSyncSignalSource(LL_CRS_SYNC_SOURCE_USB);
     LL_CRS_SetReloadCounter(__LL_CRS_CALC_CALCULATE_RELOADVALUE(48000000,1000));
-    LL_CRS_SetFreqErrorLimit(34);
-    LL_CRS_SetHSI48SmoothTrimming(64);
+    LL_CRS_SetFreqErrorLimit(LL_CRS_ERRORLIMIT_DEFAULT);
+    LL_CRS_SetHSI48SmoothTrimming(LL_CRS_HSI48CALIBRATION_DEFAULT);
     LL_CRS_EnableAutoTrimming();
     LL_CRS_EnableFreqErrorCounter();
 #endif
@@ -23,7 +24,7 @@ static void enable_crs()
 
 void main(void)
 {
-#if CONFIG_CLOCK_STM32_PLL_SRC_HSI
+#if CONFIG_SOC_STM32G474XX
     LL_DBGMCU_EnableDBGSleepMode();
     LL_DBGMCU_EnableDBGStopMode();
     LL_DBGMCU_EnableDBGStandbyMode();
